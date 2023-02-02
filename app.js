cb5f7df75093b27d173ac8dccb285433dfaa044d
Pavan5527 <vadagampavan18@gmail.com>
2023-02-02 23:00:51 +0530

    Initial commit

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const bodyParser= require("body-parser");
 const request= require("request");
 const https= require("https");
 
+const dotenv= require("dotenv");
+dotenv.config({path: './.env'});
 const app= express();
+
+const port= process.env.PORT || 3000;
+const api_key=process.env.API_KEY;
+const api_id=process.env.API_ID;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
@@ -32,7 +38,7 @@ app.post("/",function(req,res){
     const url= "https://us13.api.mailchimp.com/3.0/lists/db0014f080"; 
     const Options={
         method: "POST",
-        auth: "Pavan:29a1710ded0f69ee1634235463cf5d95-us13"
+        auth: "Pavan:8f0fe88c430f1808a6187d0bb7021214-us13"
     }
 
     const request=https.request(url,Options, function(response){
@@ -59,6 +65,6 @@ app.post("/failure",function(req,res){
     res.redirect("/");
 })
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(3000, function(){
     console.log("server is running on port 3000");
 })
